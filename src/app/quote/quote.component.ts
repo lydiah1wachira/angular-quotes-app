792c992063c70425e3987d65df1b61c723a387c1
lydiah1wachira <lydiahcedric@gmail.com>
2022-04-10 09:22:43 +0300

    create a quote-detail component.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote [] = [
+    new Quote (1, 'Too much sanity may be madness — and maddest of all: to see life as it is, and not as it should be!')
+  ]
 
   constructor() { }
 
